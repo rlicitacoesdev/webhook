ff5ef0dac3fa204908a42b69637a24fedecdd67b
rlicitacoesdev <rlicitacoes.dev@gmail.com>
2025-11-04 16:29:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // Set port and verify_token
 const port = process.env.PORT || 3000;
-const verifyToken = process.env.VERIFY_TOKEN;
+const verifyToken = 'token';
 
 // Route for GET requests
 app.get('/', (req, res) => {
@@ -26,12 +26,12 @@ app.get('/', (req, res) => {
 // Route for POST requests
 app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-  console.log(`\n\nWebhook received ${timestamp}\n`);
+  console.log(\n\nWebhook received ${timestamp}\n);
   console.log(JSON.stringify(req.body, null, 2));
   res.status(200).end();
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`\nListening on port ${port}\n`);
+  console.log(Listening on port ${port}\n );
 });
